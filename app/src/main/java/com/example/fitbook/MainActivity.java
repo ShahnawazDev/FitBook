d92e34772e84d3f469056f55331c01d72151d635
TheNPDev <theharsh1210@gmail.com>
2022-05-20 18:07:23 +0530

    two activiy are ready

--- app/src/main/java/com/example/fitbook/MainActivity.java
+++ app/src/main/java/com/example/fitbook/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.fitbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext() , home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
